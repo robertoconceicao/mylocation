86ae6b7d23d7a3e603b25f5c60f916ada4975d7f
robertoconceicao <conceicao.roberto@gmail.com>
2012-11-21 00:03:34 -0200

    atualizando o client android, add coordenadas gps e enviando parar o servidor

---
+++ MyLocationClient/src/br/com/mylocationclient/views/GpsActivity.java
@@ -0,0 +1,88 @@
+package br.com.mylocationclient.views;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import br.com.mylocation.bean.message.Event;
+import br.com.mylocation.bean.message.event.Position;
+import br.com.mylocation.define.ProtocolDefines;
+import br.com.mylocationclient.R;
+import br.com.mylocationclient.io.Client;
+
+public class GpsActivity extends Activity implements LocationListener {
+
+	private TextView latitudeText;
+	private TextView longitudeText;
+	private LocationManager locationManager;
+	
+	private Client client;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gps);
+		
+		//pega a referencia do client 
+		Bundle extras = getIntent().getExtras();
+		client = (Client) extras.getSerializable("client");
+
+		latitudeText = (TextView) findViewById(R.id.infoLatitude);
+		longitudeText = (TextView) findViewById(R.id.infoLongitude);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 400, 1, this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		latitudeText.setText(String.valueOf(location.getLatitude()));
+		longitudeText.setText(String.valueOf(location.getLongitude()));
+		
+		if(client != null) {
+			Position position = new Position(location.getLatitude(),
+											 location.getLongitude(), 
+											 location.getSpeed(), 
+											 location.getAccuracy(), 
+											 location.getAltitude(), 
+											 System.currentTimeMillis());
+			Event eventPosition = new Event(ProtocolDefines.OPERATION_POSITION, position);
+			try {
+				client.sendMessage(eventPosition);
+				Toast.makeText(this, "Enviando Position para servidor ", Toast.LENGTH_SHORT).show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+}
