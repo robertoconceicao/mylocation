050c36fc0a12707008dafd6ee6c5c3b930c77700
Johannes Ferreira <johannesssf@gmail.com>
2012-11-25 18:33:51 -0200

    Atualizando o visualizador de pontos no mapa. Para acessar, copiar o diretório MapView para /webapps do tomcat ou similar e acessar localhost:8080/MapView

---
+++ MapView/src/br/com/mylocation/servlets/UserLocationManager.java
@@ -0,0 +1,39 @@
+package br.com.mylocation.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class UserLocationManager
+ */
+@WebServlet("/UserLocationManager.do")
+public class UserLocationManager extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userCode = request.getParameter("user_code");
+		
+		// Pegamos as informações do usuário e devolvemos um JSON
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "Johannes Ferreira");
+		jsonObject.put("code", userCode);
+		jsonObject.put("latitude", -27.603608);
+		jsonObject.put("longitude", -48.576372);
+		jsonObject.put("extra", "Last place!!!");
+		
+		System.out.println("json: " + jsonObject.toJSONString());
+		
+		PrintWriter out = response.getWriter();
+		out.print(jsonObject.toString());
+		out.close();
+	}
+}
