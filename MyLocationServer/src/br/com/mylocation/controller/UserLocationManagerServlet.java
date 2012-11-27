bfdac969385665c2c948a0a39c0a12c41fdaf0a4
Johannes Ferreira <johannesssf@gmail.com>
2012-11-26 22:34:03 -0200

    Atualizando MyLocationServer.

--- MyLocationServer/src/br/com/mylocation/controller/UserLocationManagerServlet.java
+++ MyLocationServer/src/br/com/mylocation/controller/UserLocationManagerServlet.java
@@ -1,5 +1,7 @@
 package br.com.mylocation.controller;
 
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,30 +11,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class UserLocationManager
+ */
 @WebServlet(description = "Gerenciador de Localização do Usuário", urlPatterns = { "/UserLocationManagerServlet" })
 public class UserLocationManagerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+ 
+	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userCode = request.getParameter("user_code");
-
-		/*
-		 * request.setAttribute("user_info", getUserInfo(userCode));
-		 * 
-		 * RequestDispatcher dispatcher = request.getRequestDispatcher("UserLocation.jsp"); dispatcher.forward(request, response);
-		 */
-
+		
+		// Pegamos as informações do usuário e devolvemos um JSON
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "Johannes Ferreira");
+		jsonObject.put("code", userCode);
+		jsonObject.put("latitude", -27.603608);
+		jsonObject.put("longitude", -48.576372);
+		jsonObject.put("extra", "Last place!!!");
+		
+		System.out.println("json: " + jsonObject.toJSONString());
+		
 		PrintWriter out = response.getWriter();
-		out.print("klasjfadflakjflakdjfalskdfadfhadfjdfa");
+		out.print(jsonObject.toString());
 		out.close();
 	}
-
-	/*
-	 * private User getUserInfo(String userCode) { //TODO: Temos que ver como pegaremos as informações. User user = new User();
-	 * user.setName("Johannes Ferreira"); user.setCode("a34hh2gd"); user.setInfo("Alguma informação relevante."); user.setPoints(new
-	 * ArrayList<LocationInfo>()); user.getPoints().add(new LocationInfo("-27.606574", "-48.578796", null)); user.getPoints().add(new
-	 * LocationInfo("-27.603399", "-48.576307", null)); user.getPoints().add(new LocationInfo("-27.60959", "-48.578861", null));
-	 * 
-	 * return user; }
-	 */
 }
---
+++ MyLocationServer/src/br/com/mylocation/servlet/listener/MyLocationServerListener.java
@@ -0,0 +1,32 @@
+package br.com.mylocation.servlet.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import br.com.mylocation.socket.ControllerClient;
+import br.com.mylocation.socket.ServerSocket;
+
+public class MyLocationServerListener implements ServletContextListener {
+	
+	private ControllerClient controllerClient = null;
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		// TODO Auto-generated method stub
+		ServletContext context = event.getServletContext();
+		System.out.println("Iniciando sistema...");
+		controllerClient = new ControllerClient();
+		controllerClient.setServerSocket(new ServerSocket(controllerClient));
+		System.out.println("Sistema inicializado.");
+		
+		context.setAttribute("ControllerClient", controllerClient);
+	}
+
+}
